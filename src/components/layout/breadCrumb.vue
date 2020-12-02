1e5bc0ed6f35a2c77fa3087053394121ec225a65
teeqq <5570826+teeqq@user.noreply.gitee.com>
2020-12-02 17:39:01 +0800

    搭建后台框架

---
+++ src/components/layout/breadCrumb.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>breadCrumb</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <sidebar></sidebar>
+    <div>
+      <breadCrumb></breadCrumb>
+      <adminMain></adminMain>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from '@/components/layout/sideBar.vue';
+import BreadCrumb from '@/components/layout//breadCrumb.vue';
+import adminMain from '@/components/layout/adminMain.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Sidebar,
+    BreadCrumb,
+    adminMain
+  }
+}
+</script>
