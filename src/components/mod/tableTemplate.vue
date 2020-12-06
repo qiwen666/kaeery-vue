849eb668d5be19469577dbb9f0b2a6df637f8e2e
teeqq <5570826+teeqq@user.noreply.gitee.com>
2020-12-06 22:49:08 +0800

    表格封装

--- src/components/mod/tableTemplate.vue
+++ src/components/mod/tableTemplate.vue
@@ -1,20 +1,67 @@
 <template>
   <el-table
     v-loading="loading"
-    border
     :header-cell-style="{ 'background-color': '#fbfbfb' }"
     highlight-current-row
     :data="tableData"
     style="width: 100%"
   >
-    <slot></slot>
+    <!-- <slot></slot> -->
+    <!-- 复选框 -->
     <el-table-column
-      v-for="column in columns"
-      :key="column.field"
-      :prop="column.field"
-      :label="column.title"
-      :width="column.width"
+      v-if="tableOptions && tableOptions.selection"
+      type="selection"
+      width="55"
+    ></el-table-column>
+
+    <!-- 序列号 -->
+    <el-table-column
+      v-if="tableOptions && tableOptions.index"
+      :label="tableOptions && tableOptions.labelIndex"
+      type="index"
+      width="55"
+    ></el-table-column>
+
+    <!-- 基本表格 -->
+    <el-table-column
+      v-for="item in columns"
+      :key="item.prop"
+      :prop="item.prop"
+      :label="item.label"
     >
+      <template slot-scope="scope">
+        <div v-if="item.render">{{ item.render(scope.row) }}</div>
+        <slot v-else-if="item.custom" :row="scope.row"></slot>
+        <div v-else>{{ scope.row[item.prop] }}</div>
+      </template>
+    </el-table-column>
+
+    <el-table-column
+      label="操作"
+      v-if="tableOptions && tableOptions.slotContent"
+    >
+      <template slot-scope="scope">
+        <slot :data="scope"></slot>
+      </template>
+    </el-table-column>
+
+    <!-- 操作按钮 -->
+    <el-table-column
+      v-if="tableHandle.label"
+      :label="tableHandle.label"
+      width="100"
+    >
+      <template slot-scope="scope">
+        <template v-for="(item, index) in tableHandle.options">
+          <el-button
+            type="text"
+            size="small"
+            :key="index"
+            @click.native.prevent="item.method(index, scope.row)"
+            >{{ item.label }}</el-button
+          >
+        </template>
+      </template>
     </el-table-column>
   </el-table>
 </template>
@@ -24,33 +71,38 @@ export default {
   props: {
     columns: {
       type: Array,
+      default: () => {
+        return [];
+      },
+    },
+    tableData: {
+      type: Array,
+      default: () => {
+        return [];
+      },
+    },
+    tableOptions: {
+      type: Object,
+      default: {
+        stripe: false, //斑马纹
+        highlightCurrentRow: false, //高亮当前行
+        border: false,
+        index: false, //序列号,
+        labelIndex: "", //序列号文本显示
+        selection: false, //复选框
+        slotContent: false,
+      },
+    },
+    tableHandle: {
+      type: Object,
+      default: () => {
+        return {};
+      },
     },
   },
   data() {
     return {
       loading: false,
-      tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄",
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄",
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄",
-        },
-      ],
     };
   },
 };
--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -5,14 +5,20 @@
       <el-button plain icon="el-icon-search">搜索</el-button>
     </div>
     <table-template 
-    :columns="table.columns"
+    :columns="tableHeader"
+    :tableOptions="tableOptions"
+    :tableHandle="tableHandle"
     :tableData="tableData"
+    v-slot="slotProps"
     >
-      <el-table-column label="操作" width="120">
-        <template slot-scope="scope">
-          <el-button type="text" size="small">移除 </el-button>
-        </template>
-      </el-table-column>
+    <el-tooltip
+      class="item"
+      effect="dark"
+      :content="slotProps.row.content"
+      placement="bottom"
+    >
+      <div>{{ slotProps.row.content | toFormatContent }}</div>
+    </el-tooltip>
     </table-template>
   </div>
 </template>
@@ -21,25 +27,104 @@
 import tableTemplate from "@/components/mod/tableTemplate";
 
 export default {
+  filters: {
+    toFormatContent(val) {
+      return val.length > 12 ? val.slice(0,12) + '...' : val
+    }
+  },
   data() {
     return {
       keyword: "",
-      table: {
-        columns: [{
-          prop: 'cover',
-          label: '封面'
-        },{
-          prop: 'startTime',
-          label: '开始时间'
-        },{
-          prop: 'endTime',
-          label: '结束时间'
-        },{
-          prop: 'tel',
-          label: '联系电话'
-        }],
+      // table配置
+      tableHeader: [
+        {
+          label: '公司名称',
+          prop: 'title'
+        },
+        {
+          label: '注册时间',
+          prop: 'registerTime'
+        },
+        {
+          label: '内容',
+          prop: 'content',
+          custom: true
+        },
+        {
+          label: '审核时间',
+          prop: 'status',
+          render: (row) => row.status === 1 ? '已发布' : row.status === 2 ? '发布中' : '未发布'
+        }
+      ],
+      tableOptions: {
+        selection: false,
+        index: true,
+        labelIndex: '序号'
       },
-    };
+      tableHandle: {
+        label: '操作',
+        options: [
+          {
+            label: '修改',
+            method: (index,row) => {
+              this.handleEdit(index,row);
+            }
+          },
+          {
+            label: '删除',
+            method: (index,row) => {
+              this.handleDel(index,row);
+            }
+          }
+        ]
+      },
+      /**
+       * // 表格操作按钮
+const operates = that => [
+  {
+    label: "编辑",
+    isShow: row => {
+      return row.status !== 2;
+    },
+    disabled: row => {
+      return row.disabled === 2;
+    },
+    method: row => {
+      that.handleNewJump(row);
+    }
+  }
+];
+       */
+      tableData: [
+        {
+          registerTime: "2016-05-02",
+          title: "王小虎",
+          status: 1,
+          content: '意见反馈意见反馈意见反馈意见反馈意见反馈意见反馈'
+        },
+        {
+          registerTime: "2016-05-02",
+          title: "王小虎",
+          status: 2,
+          content: '意见反馈意见反馈意见反馈意见反馈意见反馈意见反馈'
+
+        },
+        {
+          registerTime: "2016-05-02",
+          title: "王小虎",
+          status: 3,
+          content: '意见反馈意见反馈意见反馈意见反馈意见反馈意见反馈'
+
+        },
+        {
+          registerTime: "2016-05-02",
+          title: "王小虎",
+          status: 1,
+          content: '意见反馈意见反馈意见反馈意见反馈意见反馈意见反馈'
+
+        },
+      ],
+    }
   },
   mounted() {
     let breadcrumb = [
@@ -53,6 +138,14 @@ export default {
       },
     ];
     this.$store.commit("SET_BREADCRUMB", breadcrumb);
+  },
+  methods: {
+    handleEdit(index,row) {
+      console.log(index,row,'编辑');
+    },
+    handleDel(index,row) {
+      console.log(index,row,'删除');
+    }
   },
   components: {
     tableTemplate,
